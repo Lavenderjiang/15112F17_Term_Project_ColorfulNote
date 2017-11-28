823f33318ccd1739cd733ce0e8909c60b0c6291a
Lavenderjiang <lavenderchiang@gmail.com>
2017-11-28 13:02:35 -0500

    better documentation

--- AudioProcessing/test.py
+++ AudioProcessing/test.py
@@ -1,8 +1,18 @@
+#author: Lavender Jiang
+#andrewID: yaoj
+#15112F17 Term Project
+
+################################################################################
+############################### Imports ########################################
+################################################################################
+
+'''Dependencies: pyaudio, matplotlib, numpy'''
 import pyaudio
 from array import array
 from struct import pack #used to unpack audio data into integer
 from ast import literal_eval
 from math import log2
+import decimal
 from random import uniform
 from random import randint
 from pylab import *
@@ -16,28 +26,122 @@ import wave
 import struct
 fft = np.fft.fft
 
+################################################################################
+############################### Helpers ########################################
+################################################################################
+
 #copied from matploblib official tutorial
 def make_ticklabels_invisible(fig):
     for i, ax in enumerate(fig.axes):
         ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
         for tl in ax.get_xticklabels() + ax.get_yticklabels():
             tl.set_visible(False)
+            
+#copied from 112 course website
+def roundHalfUp(d):
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
+
+def fToNote(f):
+    '''
+    f Range: A0(27.5Hz) ~ G5(783Hz)
+    @param f: frequency in range
+    @return: a string of English notation of musical notes (e.g. A4)
+    '''
+    f = np.asscalar(f) #convert to scalar
+    C0 = 16.35
+    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    halfSteps = roundHalfUp(12*log2(f/C0)) #formula taken from John D. Cook
+    octave = halfSteps/12
+    pitch = notes[halfSteps%12]
+    return "%s%d"%(pitch,octave)
+
+def testFConversion():
+    print(fToNote(440)) #A4
+    print(fToNote(391)) #G4
+    print(fToNote(800)) #G5
+    print(fToNote(743)) #Fsharp5
+
+def fToAngle(f):
+    '''
+    f Range: A0(27.5Hz) ~ G5(783Hz)
+    @param f: frequency in range
+    @return: the polar angle of a circle in radians
+    '''
+    unitAngle = 2*np.pi/59
+    C0 = 16.35
+    halfStepsToC0 = roundHalfUp(12*log2(f/C0))
+    halfStepsToA0 = halfStepsToC0 - 9
+    startAngle = unitAngle * halfStepsToA0
+    fAngle = uniform(startAngle,startAngle+unitAngle)
+    return fAngle
+
+def testAngleConversion():
+    fToAngle(800) #around 360 degree
+    fToAngle(27.5) #around 0 degree
+    fToAngle(400) #around 300 degree
+
+def convertToArea(stren):
+    '''
+    stren Range: 0 ~ 70
+    @param stren: volume/amplitude of current sample
+    @return: the area of a circle
+    '''
+    threshold = 5
+    #sound with volume below threshold is considered noise 
+    if stren<threshold: return 5
+    #generate random value in specified range
+    elif stren<30: res = randint(5,30)
+    elif stren<50: res = randint(30,50)
+    elif stren<60: res = randint(50,100)
+    else: res = randint(100,150)
+    return res
+    
+def testAreaConversion():
+    print(convertToArea(42.7)) #('low',5)
+    print(convertToArea(68.9)) #('high', 20)
+    
+def extractData():
+    '''Write frequency and strength data to .txt file.'''
+    f = open("data.txt","r")
+    res = []
+    #use literal_eval to safety turn string back to tuple
+    for line in f:
+        res.append(literal_eval(line))
+    #first is frequency(0~800), second is strength(0~70)
+    for d in res:
+        freq = d[0]
+        pitch = fToNote(freq)
+        stren = d[1]
+    
+################################################################################
+################################## Main ########################################
+################################################################################
 
 def debugPlot():
-    #ax  --> volume over time
-    #ax2 --> volume over freq
-    #ax3  --> drawing
+    '''
+    The main fucntion for displaying real-time drawing
+    @var ax:  np line of volume over time
+    @var ax2: np line of volume over frequency
+    @var ax3: np polar graph of generated painting
+    '''
+    
 ################################################################################
 ############################### Constants ######################################
 ################################################################################
+    
     offset = 157
     startR = 0.1
     stepR = 0.1
     curTime = 0
+    
 ################################################################################
 ############################ Interaction Class #################################
 ################################################################################
+    
     class MouseStop:
+        '''stop the animation loop when the user clicks'''
         def __init__(self, line):
             self.line = line
             self.stop = False
@@ -49,31 +153,7 @@ def debugPlot():
             print('click', event)
             self.stop = True
             print('stop',self.stop)
-###########################################################################
-    #set the handdrawn style      
-    with plt.xkcd():
-
-        fig = plt.figure()
-        
-        gs = GridSpec(5, 5)
-        gs.update(wspace=0.05)
-        # ax = plt.subplot(gs[0, :])
-        # identical to ax1 = plt.subplot(gs.new_subplotspec((0,0), colspan=3))
-        ax2 = plt.subplot(gs[:, -1:])
-        ax3 = plt.subplot(gs[:,:-1], polar=True,autoscale_on=False)
-        ax3.axis('off')
-        # ax3.plot(theta, r)
-        ax3.grid(True)
-        pitchText = ax2.text(0.05, 0.9, '')
-        
-        r = []
-        theta = []
-        area = []
-        colors = theta 
     
-        ax3.scatter(theta, r, c=colors, s=area, cmap=cm.cool)
-        ax3.set_alpha(0.5) #alpha is the ratio of transparency
-
 ################################################################################
 ############################### PyAudio ########################################
 ################################################################################
@@ -96,37 +176,55 @@ def debugPlot():
 ################################################################################
 ########################## MPL Initialization ##################################
 ################################################################################
-    mpl.rcParams['toolbar'] = 'None' #disable ugly default toolbar
-    # variable for plotting
-    x = np.arange(0, 2 * CHUNK, 2)
+    
+    r = []
+    theta = []
+    area = []
+    colors = theta 
     x_fft = np.linspace(0,RATE,CHUNK) #max freq is sampling rate 
     
-    # create a line object with random data
-    #line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
-    line_fft, = ax2.plot(np.random.rand(CHUNK),x_fft,  '-', lw=2)
-    curFreq = 0
-    
-    
-    first = MouseStop(line_fft)
-    ax2.set_ylim(20,RATE/20) #at 0 line is discontinuous
-    
-    
-    #fig.tight_layout()
-    make_ticklabels_invisible(fig)
-    plt.show(block=False)
+    #set the handdrawn style      
+    with plt.xkcd():
+
+        fig = plt.figure()
+        
+        #arrange subplot positions using gridspec
+        gs = GridSpec(5, 5)
+        gs.update(wspace=0.05)
+        ax2 = plt.subplot(gs[:, -1:])
+        ax3 = plt.subplot(gs[:,:-1], polar=True,autoscale_on=False)
+        
+        #configue ax2 and ax3
+        ax2.set_ylim(20,RATE/20) #at 0 line is discontinuous
+        ax3.axis('off')
+        ax3.grid(True)
+        ax3.scatter(theta, r, c=colors, s=area, cmap=cm.cool)
+        ax3.set_alpha(0.5) #alpha is the ratio of transparency
+        
+        #create line and text objects to be updated
+        pitchText = ax2.text(0.05, 0.9, '')
+        line_fft, = ax2.plot(np.random.rand(CHUNK),x_fft,  '-', lw=2)
+       
+        #bind click action with subplot
+        first = MouseStop(line_fft)
+        
+        #configue the entire figure
+        fig.tight_layout()
+        mpl.rcParams['toolbar'] = 'None' #disable ugly default toolbar
+        make_ticklabels_invisible(fig)
+        plt.show(block=False)
     
     print('stream started')
+    
 ################################################################################
 ########################## Animation Loop ######################################
 ################################################################################    
+    
     res=[]
     while first.stop == False:
-        #print("in while loop!")
         data = stream.read(CHUNK,exception_on_overflow = False)
         data_int = struct.unpack(str(2*CHUNK)+'B',data)
-        #print(data_int)
         data_np = np.array(data_int, dtype='b')[::2] 
-        #print(data_np)
         #line.set_ydata(data_np)
         #fft
         y_fft = fft(data_int)
@@ -187,76 +285,3 @@ def debugPlot():
         f.write("\n")
     f.close()
 
-#copied from 112 course website
-import decimal
-def roundHalfUp(d):
-    # Round to nearest with ties going away from zero.
-    rounding = decimal.ROUND_HALF_UP
-    # See other rounding options here:
-    # https://docs.python.org/3/library/decimal.html#rounding-modes
-    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
-
-#Detection Range: A0(27.5Hz) ~ G5(783Hz)
-#59 half steps in total
-def fToNote(f):
-    f = np.asscalar(f) #convert to scalar
-    C0 = 16.35
-    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-    halfSteps = roundHalfUp(12*log2(f/C0)) #formula taken from John D. Cook
-    octave = halfSteps/12
-    pitch = notes[halfSteps%12]
-    return "%s%d"%(pitch,octave)
-
-def testFConversion():
-    print(fToNote(440)) #A4
-    print(fToNote(391)) #G4
-    print(fToNote(800)) #G5
-    print(fToNote(743)) #Fsharp5
-
-def fToAngle(f):
-    unitAngle = 2*np.pi/59
-    print(unitAngle)
-    C0 = 16.35
-    halfStepsToC0 = roundHalfUp(12*log2(f/C0))
-    halfStepsToA0 = halfStepsToC0 - 9
-    startAngle = unitAngle * halfStepsToA0
-    #fAngle = randint(0,2*np.pi)
-    #print(startAngle)
-    # print(startAngle+unitAngle)
-    fAngle = uniform(startAngle,startAngle+unitAngle)
-    #print("degree:",360/(2*np.pi)*fAngle)
-    return fAngle
-
-def testAngleConversion():
-    fToAngle(800) #around 360 degree
-    fToAngle(27.5) #around 0 degree
-    fToAngle(400) #around 300 degree
-
-def convertToArea(stren):
-    threshold = 5
-    loudness = ["tiny","low","medium","high"]
-    radii = [30,50,100,150]
-    if stren<5: return 5
-    elif stren<30: res = randint(5,30)
-    elif stren<50: res = randint(30,50)
-    elif stren<60: res = randint(50,100)
-    else: res = randint(100,150)  
-    return res
-    
-def testAreaConversion():
-    print(convertToArea(42.7)) #('low',5)
-    print(convertToArea(68.9)) #('high', 20)
-    
-def extractData():
-    f = open("data.txt","r")
-    res = []
-    #use literal_eval to safety turn string back to tuple
-    for line in f:
-        res.append(literal_eval(line))
-    #first is frequency(0~800), second is strength(0~64)
-    for d in res:
-        freq = d[0]
-        pitch = fToNote(freq)
-        print(pitch)
-        stren = d[1]
-    #print(res)
